d194e0e2fde373f5f4f2bd5d342a06de6de75e8c
Diger <kitten.diger@gmail.com>
2024-01-16 16:19:36 +0900

    tempˆ

---
+++ src/user-service/src/main/java/org/palette/easeluserservice/service/UserService.java
@@ -0,0 +1,9 @@
+package org.palette.easeluserservice.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+}
